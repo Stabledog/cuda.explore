30a6c4b1b649b7fc53ee5551076b51260233fb0f
Les Matheson <les.matheson@gmail.com>
2024-02-19 17:26:50 -0500

    Switch to different tutorial source

--- tutorial01/vector_add.cu
+++ tutorial01/vector_add.cu
@@ -1,25 +1,38 @@
-// cuda_hello.c
+#include <iostream>
+#include <math.h>
 
-
-#include <stdio.h>
-#define N 10000000
-
-__global__ void vector_add(float *out, float *a, float *b, int n) {
-    for (int i = 0; i < n; i++) {
-        out[i] = a[i] + b[i];
-    }
+// function to add the elements of two arrays
+void add(int n, float *x, float *y)
+{
+  for (int i = 0; i < n; i++)
+      y[i] = x[i] + y[i];
 }
 
-int main() {
-    float *a = (float*)malloc(sizeof(float) * N);
-    float *b = (float*)malloc(sizeof(float) * N);
-    float *out = (float*)malloc(sizeof(float) * N);
-    for (int i = 0; i < N; ++i) {
-        a[i] = 1.0f; b[i] = 2.0f;
-    }
-    vector_add<<<1,1>>>(out,a,b,N);
-    for (int i = 10; i < 20; ++i) {
-        printf("%f\n", out[i]);
-    }
-    return 0;
+int main(void)
+{
+  int N = 1<<20; // 1M elements
+
+  float *x = new float[N];
+  float *y = new float[N];
+
+  // initialize x and y arrays on the host
+  for (int i = 0; i < N; i++) {
+    x[i] = 1.0f;
+    y[i] = 2.0f;
+  }
+
+  // Run kernel on 1M elements on the CPU
+  add(N, x, y);
+
+  // Check for errors (all values should be 3.0f)
+  float maxError = 0.0f;
+  for (int i = 0; i < N; i++)
+    maxError = fmax(maxError, fabs(y[i]-3.0f));
+  std::cout << "Max error: " << maxError << std::endl;
+
+  // Free memory
+  delete [] x;
+  delete [] y;
+
+  return 0;
 }
